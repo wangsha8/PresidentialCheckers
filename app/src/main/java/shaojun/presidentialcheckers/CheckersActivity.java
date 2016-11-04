59dba4b72fe63224136cd06b3de1f792602a6207
shaojun <shaojunw@outlook.com>
2016-11-03 23:52:17 -0400

    Revision

---
+++ app/src/main/java/shaojun/presidentialcheckers/CheckersActivity.java
@@ -0,0 +1,13 @@
+package shaojun.presidentialcheckers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CheckersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checkers);
+    }
+}
